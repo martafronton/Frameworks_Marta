022dc028ab07c1da13bbfd64ac194bffebe0ee8a
martafronton <martafronton@gmail.com>
2025-09-15 17:45:25 +0200

    Ejercicio ficheros
    El programa lee los datos de Libro2.txt y genera un fichero de logs con fecha actual llamado DDMMYYYY_Personas.log.

---
+++ EjercicioFicheros/main.php.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+
+ruta = "C:\\Users\\Marta\\Downloads\\Libro2.txt"
+
+fecha = datetime.now().strftime("%d%m%Y")
+
+with open(ruta, "r") as f1:
+    lineas = f1.readlines()
+
+
+with open(f"{fecha}_Personas.log", "a") as f2:
+    for fila in lineas:
+
+        datos = fila.split()
+
+
+        f2.write(
+            f'Insert Personas (id,Nombre, Apellidos, fecha_nacimiento, calle, codigo_postal, numero, movil) '
+            f'values (seq_personas.nextval, "{datos[0]}", "{datos[1]}", "{datos[2]}", "{datos[3]}", "{datos[4]}", "{datos[5]}", "{datos[6]}");\n'
+        )
