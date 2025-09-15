a701177c13504c110b5a101ca0192120ac9d9749
alion1992 <55086590+alion1992@users.noreply.github.com>
2025-09-15 19:05:23 +0200

    Update main.php.py

--- EjercicioFicheros/main.php.py
+++ EjercicioFicheros/main.php.py
@@ -19,3 +19,5 @@ with open(f"{fecha}_Personas.log", "a") as f2:
             f'Insert Personas (id,Nombre, Apellidos, fecha_nacimiento, calle, codigo_postal, numero, movil) '
             f'values (seq_personas.nextval, "{datos[0]}", "{datos[1]}", "{datos[2]}", "{datos[3]}", "{datos[4]}", "{datos[5]}", "{datos[6]}");\n'
         )
+
+#rutas relativas nunca absolutas. create una carpeta log y lo metes dentro el nuevo archivo. y cuidado con main.php.py php no me gusta mucho
